d685be18c7c25907070559d6df2648dc6395b997
rafaelmdalmoro <rafamascmoro@hotmail.com>
2021-09-13 19:37:45 -0300

    teste 3 - X vence todas
    testa todos os casos em que o X vence, retornando sempre 1
    Teste Passou!
    Obs: implementadas matrizes vitoriaX_<n>[3][3]

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -16,5 +16,40 @@ TEST_CASE("Testa velha", "[single-file]") {
 	                          { 2, 0, 1 },
 						      { 0, 2, 1 }
     };
+	int vitoriaX_2[3][3]= {   { 2, 1, 2 },
+	                          { 2, 1, 0 },
+						      { 0, 1, 2 }
+    };
+	int vitoriaX_3[3][3]= {   { 1, 0, 2 },
+	                          { 1, 2, 0 },
+						      { 1, 2, 2 }
+    };
+	int vitoriaX_4[3][3]= {   { 1, 0, 2 },
+	                          { 2, 1, 0 },
+						      { 2, 2, 1 }
+    };
+	int vitoriaX_5[3][3]= {   { 2, 0, 1 },
+	                          { 2, 1, 0 },
+						      { 1, 2, 2 }
+    };
+	int vitoriaX_6[3][3]= {   { 1, 1, 1 },
+	                          { 2, 0, 0 },
+						      { 0, 2, 2 }
+    };
+	int vitoriaX_7[3][3]= {   { 0, 2, 0 },
+	                          { 1, 1, 1 },
+						      { 0, 2, 2 }
+    };
+	int vitoriaX_8[3][3]= {   { 0, 2, 0 },
+	                          { 2, 0, 2 },
+						      { 1, 1, 1 }
+    };
     REQUIRE(VerificaVelha(vitoriaX_1) == 1);
+	REQUIRE(VerificaVelha(vitoriaX_2) == 1);
+	REQUIRE(VerificaVelha(vitoriaX_3) == 1);
+	REQUIRE(VerificaVelha(vitoriaX_4) == 1);
+	REQUIRE(VerificaVelha(vitoriaX_5) == 1);
+	REQUIRE(VerificaVelha(vitoriaX_6) == 1);
+	REQUIRE(VerificaVelha(vitoriaX_7) == 1);
+	REQUIRE(VerificaVelha(vitoriaX_8) == 1);
 }
